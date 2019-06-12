2121bb02e5e1a55c4b2190aeac9aca6b213c8f4c
Matthew Coon <46505830+mattc8818@users.noreply.github.com>
2019-06-12 10:43:36 -0400

    Rewrote methods to be oriented around AIS stitching rather than file reading and writing.
    This works well when sequence values are unique, but when there are reused AISs, they are not played or added to the stream.

--- Clipper.java
+++ Clipper.java
@@ -28,8 +28,8 @@ public class Clipper {
 	/*
 	 * Size of 12 String array that stores String filepaths for clipped WAVs.
 	 * 
-	 * clippingsURLStr[0 - 7] correspond to e1 - e8 WAV files. 
-	 * clippingsURLStr[8-11] correspond to q1 - q4 WAV files.
+	 * clippingsURLStr[0 - 7] correspond to e1 - e8 WAV files. clippingsURLStr[8-11]
+	 * correspond to q1 - q4 WAV files.
 	 */
 	private String[] clippingsURLStr;
 
@@ -37,6 +37,9 @@ public class Clipper {
 
 	public static Mixer mixer;
 	public static Clip clip;
+
+	private AudioInputStream[] eighthsOfClip;
+	private AudioInputStream[] quartersOfClip;
 
 	public Clipper(String clipperName, String sourceURLStr, String destURLStr) {
 		this.clipperName = clipperName;
@@ -53,8 +56,11 @@ public class Clipper {
 
 		lengthMS = getLengthMS(sourceURL);
 
-		// Initializes split clips of the file, and recieves their respective filepath.
-		clippingsURLStr = initSplitFiles();
+		eighthsOfClip = initEighthsAIS();
+		quartersOfClip = initQuartersAIS();
+
+		// Initializes split clips of the file, and receives their respective filepath.
+		// clippingsURLStr = initSplitFiles();
 	}
 
 	/*
@@ -62,7 +68,7 @@ public class Clipper {
 	 * 
 	 * Outputs a String array with the URL Strings of the resulting files.
 	 */
-	public String[] initSplitFiles() {
+	public String[] createSplitFiles() {
 		String[] clippingsFilesURLStr = new String[12];
 
 		String clipperFolderFilePath = destURLStr + clipperName + "-Clips/";
@@ -70,8 +76,10 @@ public class Clipper {
 
 		boolean success = (new File(clipperFolderFilePath)).mkdirs();
 		success = (new File(stitchesFolderFilePath)).mkdirs();
-		if (!success) { System.out.println("Possible problems with your generated folders"); }
-		
+		if (!success) {
+			System.out.println("Possible problems with your generated folders");
+		}
+
 		int fileLenEighth = lengthMS / 8;
 
 		int[] startPts = new int[8];
@@ -100,7 +108,42 @@ public class Clipper {
 		return clippingsFilesURLStr;
 	}
 
-	public int getLengthMS(URL sourceURL) {
+	
+	/*
+	 * This method initializes the AudioInputStreams of eight pieces of the file, so 
+	 * that they may be sequenced later.
+	 */
+	public AudioInputStream[] initEighthsAIS() {
+		AudioInputStream[] eighthsClips = new AudioInputStream[8];
+
+		int eClipLen = (lengthMS / 8);
+
+		for (int x = 0; x < eighthsClips.length; x++) {
+			eighthsClips[x] = Clipper.shortenedStream(sourceURLStr, eClipLen * x, eClipLen);
+		}
+
+		return eighthsClips;
+	}
+	
+	/*
+	 * This method initializes the AISs of four pieces of the file, for sequencing.
+	 */
+	public AudioInputStream[] initQuartersAIS() {
+		AudioInputStream[] quartersClips = new AudioInputStream[8];
+
+		int qClipLen = (lengthMS / 4);
+
+		for (int x = 0; x < quartersClips.length; x++) {
+			quartersClips[x] = Clipper.shortenedStream(sourceURLStr, qClipLen * x, qClipLen);
+		}
+
+		return quartersClips;
+	}
+
+	/*
+	 * Outputs the ms of the WAV file.
+	 */
+	public static int getLengthMS(URL sourceURL) {
 		Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
 
 		mixer = AudioSystem.getMixer(mixInfos[0]);
@@ -122,62 +165,11 @@ public class Clipper {
 		} catch (LineUnavailableException e) {
 			e.printStackTrace();
 		}
-
 		int lengthMS = (int) (clip.getMicrosecondLength() * 0.001);
 
 		return lengthMS;
 	}
 
-	/*
-	 * Method creates an audio file based on a clip of another.
-	 * 
-	 * Code from: https://tinyurl.com/y6h9qloa
-	 */
-	public static void copyAudio(String sourceFileName, String destinationFileName, int startSecond,
-			int secondsToCopy) {
-		AudioInputStream inputStream = null;
-		AudioInputStream shortenedStream = null;
-		try {
-			// Reads an input file and it's file type.
-			File file = new File(sourceFileName);
-			AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
-			AudioFormat format = fileFormat.getFormat();
-
-			// Initializes the input stream.
-			inputStream = AudioSystem.getAudioInputStream(file);
-
-			// Finds the bytes per second, used to begin reading bytes.
-			int bytesPerSecond = format.getFrameSize() * (int) format.getFrameRate();
-
-			// Skips to that point of the file.
-			inputStream.skip(startSecond * bytesPerSecond);
-
-			// Finds the end point to end reading bytes.
-			long framesOfAudioToCopy = secondsToCopy * (int) format.getFrameRate();
-			System.out.println("frames to copy: " + framesOfAudioToCopy);
-
-			// Initializes the shortened stream of the clip.
-			shortenedStream = new AudioInputStream(inputStream, format, framesOfAudioToCopy);
-
-			File destinationFile = new File(destinationFileName);
-			AudioSystem.write(shortenedStream, fileFormat.getType(), destinationFile);
-		} catch (Exception e) {
-			System.out.println(e);
-		} finally {
-			if (inputStream != null)
-				try {
-					inputStream.close();
-				} catch (Exception e) {
-					System.out.println(e);
-				}
-			if (shortenedStream != null)
-				try {
-					shortenedStream.close();
-				} catch (Exception e) {
-					System.out.println(e);
-				}
-		}
-	}
 
 	/*
 	 * Method creates an audio file based on a clip of another, trims by
@@ -229,100 +221,135 @@ public class Clipper {
 		}
 	}
 
-	public void stitch(String[] sequence) {
-		AudioInputStream aisOutput = null;
+	/*
+	 * This method returns an AudioInputStream that holds a selection of a given WAV file.
+	 */
+	public static AudioInputStream shortenedStream(String sourceFileName, int startMsecs, int msecsToCopy) {
+		AudioInputStream inputStream = null;
+		AudioInputStream shortenedStream = null;
+		try {
+			// Reads an input file and it's file type.
+			File file = new File(sourceFileName);
+			AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(file);
+			AudioFormat format = fileFormat.getFormat();
+
+			// Initializes the input stream.
+			inputStream = AudioSystem.getAudioInputStream(file);
+
+			// Finds the skip point to begin reading bytes.
+			int bytesPerMsecond = (int) (format.getFrameSize() * format.getFrameRate() * 0.001);
+
+			// Skips to that point of the file. ADJUST HERE
+			inputStream.skip(startMsecs * bytesPerMsecond);
+
+			// Finds the end point to end reading bytes. ADJUST HERE
+			long framesOfAudioToCopy = (int) (msecsToCopy * format.getFrameRate() * 0.001);
+
+			// Initializes the shortened stream of the clip.
+			shortenedStream = new AudioInputStream(inputStream, format, framesOfAudioToCopy);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return shortenedStream;
+	}
+	
+	/*
+	 * Stitch takes a sequence, which is an array of strings, and plays it back.
+	 * If the parameter boolean recordStitch is true, the sequence will output a 
+	 * WAV file of the sequence.
+	 */
+	public AudioInputStream stitch(String[] sequence, boolean recordStitch) {
+		AudioInputStream appendedFiles = null;
 		
 		if (sequence.length < 2) {
 			System.out.println("Error: at least two sequences are needed to stitch.");
-			return;
+			return appendedFiles;
 		}
 		// Sequences with a length greater than 12 are not allowed.
 		if (sequence.length > 12) {
 			System.out.println("Error: sequence too long, could not stitch.");
-			return;
+			return appendedFiles;
 		}
-		
+
 		// Initializes the new stitche's string URL name.
 		String newStitchURLStr = destURLStr + clipperName + "-Stitches/";
 		for (int x = 0; x < sequence.length; x++) {
 			newStitchURLStr += sequence[x];
 		}
 		newStitchURLStr += ".wav";
-		
+
 		// Initial stitch of the first and second clip.
 		try {
-			String firstClipURLStr = getClipStrPath(sequence[0]);
-			String secondClipURLStr = getClipStrPath(sequence[1]);
+			AudioInputStream clip1 = getClip(sequence[0]);
+			AudioInputStream clip2 = getClip(sequence[1]);
+
+			appendedFiles = new AudioInputStream(new SequenceInputStream(clip1, clip2),
+					clip1.getFormat(), clip1.getFrameLength() + clip2.getFrameLength());
 			
-			AudioInputStream clip1 = AudioSystem.getAudioInputStream(new File(firstClipURLStr));
-			AudioInputStream clip2 = AudioSystem.getAudioInputStream(new File(secondClipURLStr));
-			
-			AudioInputStream appendedFiles = 
-                    new AudioInputStream(
-                        new SequenceInputStream(clip1, clip2),     
-                        clip1.getFormat(), 
-                        clip1.getFrameLength() + clip2.getFrameLength());
-			
+			// Appends additional sequenced clips, if necessary. 
 			for (int n = 2; n < sequence.length; n++) {
-				String stitchAppend = getClipStrPath(sequence[n]);
-				AudioInputStream newClip = AudioSystem.getAudioInputStream(new File(stitchAppend));
-
-				appendedFiles = new AudioInputStream(
-	                        new SequenceInputStream(appendedFiles, newClip),     
-	                        appendedFiles.getFormat(), 
-	                        appendedFiles.getFrameLength() + newClip.getFrameLength());
-
+				AudioInputStream newClip = getClip(sequence[n]);
+				
+				appendedFiles = new AudioInputStream(new SequenceInputStream(appendedFiles, newClip),
+						appendedFiles.getFormat(), appendedFiles.getFrameLength() + newClip.getFrameLength());
+				System.out.println(n);
 			}
-			
-			AudioSystem.write(appendedFiles, 
-                    AudioFileFormat.Type.WAVE, 
-                    new File(newStitchURLStr));
-		} catch (Exception e) { e.printStackTrace(); }
+			// If recordStitch is true, a file is generated for that sequence.
+			if (recordStitch) {
+				AudioSystem.write(appendedFiles, AudioFileFormat.Type.WAVE, new File(newStitchURLStr));
+				System.out.println("Sequence written to file:");
+				System.out.println(newStitchURLStr);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return appendedFiles;
 	}
-	
-	public String getClipStrPath (String clipID) {
-		String clipPath = "";
+
+	public AudioInputStream getClip(String clipID) {
+		AudioInputStream clip = null;
 		switch (clipID) {
 		case "e1":
-			clipPath = clippingsURLStr[0];
+			clip = eighthsOfClip[0];
 			break;
 		case "e2":
-			clipPath = clippingsURLStr[1];
+			clip = eighthsOfClip[1];
 			break;
 		case "e3":
-			clipPath = clippingsURLStr[2];
+			clip = eighthsOfClip[2];
 			break;
 		case "e4":
-			clipPath = clippingsURLStr[3];
+			clip = eighthsOfClip[3];
 			break;
 		case "e5":
-			clipPath = clippingsURLStr[4];
+			clip = eighthsOfClip[4];
 			break;
 		case "e6":
-			clipPath = clippingsURLStr[5];
+			clip = eighthsOfClip[5];
 			break;
 		case "e7":
-			clipPath = clippingsURLStr[6];
+			clip = eighthsOfClip[6];
 			break;
 		case "e8":
-			clipPath = clippingsURLStr[7];
+			clip = eighthsOfClip[7];
 			break;
 		case "q1":
-			clipPath = clippingsURLStr[8];
+			clip = quartersOfClip[0];
 			break;
 		case "q2":
-			clipPath = clippingsURLStr[9];
+			clip = quartersOfClip[1];
 			break;
 		case "q3":
-			clipPath = clippingsURLStr[10];
+			clip = quartersOfClip[2];
 			break;
 		case "q4":
-			clipPath = clippingsURLStr[11];
+			clip = quartersOfClip[3];
 			break;
 		default:
 			System.out.println("Error: invalid input clip ID " + clipID);
 			break;
 		}
-		return clipPath;
+		return clip;
 	}
 }
